6c363f7c7dc4662fa383d6bdb6e1190bdea664ee
warrieka <kaywarrie@gmail.com>
2015-02-10 21:06:33 +0100

    automated building icons

---
+++ images/makeIcos.bat
@@ -0,0 +1,23 @@
+REM requeres http://www.imagemagick.org/
+
+REM png for addins xml
+convert -background none geopuntAddress.svg -resize 16x16  geopuntAddress16x16.png
+convert -background none geopuntCatalog.svg -resize 16x16  geopuntCatalog16x16.png
+convert -background none geopuntPerceel.svg -resize 16x16  geopuntPerceel16x16.png
+convert -background none geopuntBatch.svg -resize 16x16  geopuntBatch16x16.png
+convert -background none geopuntElevation.svg -resize 16x16  geopuntElevation16x16.png
+convert -background none geopuntGipod.svg -resize 16x16  geopuntGipod16x16.png
+convert -background none geopuntPoi.svg -resize 16x16  geopuntPoi16x16.png
+convert -background none geopuntReverse.svg -resize 16x16  geopuntReverse16x16.png
+convert -background none geopuntSetting.svg -resize 16x16  geopuntSetting16x16.png
+
+REM ico for forms
+convert -background none geopuntAddress.svg -resize 16x16  geopuntAddress.ico
+convert -background none geopuntCatalog.svg -resize 16x16  geopuntCatalog.ico
+convert -background none geopuntPerceel.svg -resize 16x16  geopuntPerceel.ico
+convert -background none geopuntBatch.svg -resize 16x16  geopuntBatch.ico
+convert -background none geopuntElevation.svg -resize 16x16  geopuntElevation.ico
+convert -background none geopuntGipod.svg -resize 16x16  geopuntGipod.ico
+convert -background none geopuntPoi.svg -resize 16x16  geopuntPoi.ico
+convert -background none geopuntReverse.svg -resize 16x16  geopuntReverse.ico
+convert -background none geopuntSetting.svg -resize 16x16  geopuntSetting.ico
